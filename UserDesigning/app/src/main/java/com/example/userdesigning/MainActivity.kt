2ca96981e485c38d934c406d22a82241bf3231fb
guru <info.gurukamalchawla@gmail.com>
2020-01-28 12:34:04 +0530

    hello

--- UserDesigning/app/src/main/java/com/example/userdesigning/MainActivity.kt
+++ UserDesigning/app/src/main/java/com/example/userdesigning/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.userdesigning
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val DropDown=findViewById<ImageView>(R.id.iv_image);
+        DropDown.setOnClickListener{
+            DropDown.isEnabled(true)
+        }
+
     }
+
 }
+
+private fun ImageView.isEnabled(b: Boolean) {
+    visibility = if (b) {
+        ImageView.VISIBLE
+    } else
+        ImageView.INVISIBLE
+}
+
+
